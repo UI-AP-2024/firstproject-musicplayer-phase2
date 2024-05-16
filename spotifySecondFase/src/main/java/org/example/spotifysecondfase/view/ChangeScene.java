466b5d8a625322bc6db96c8413800dbae2a7d115
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-05-16 22:05:22 +0330

    tabe baraye taghir safe be safhe chooseGenres

--- spotifySecondFase/src/main/java/org/example/spotifysecondfase/view/ChangeScene.java
+++ spotifySecondFase/src/main/java/org/example/spotifysecondfase/view/ChangeScene.java
@@ -9,10 +9,25 @@ import java.io.IOException;
 public class ChangeScene {
     static Stage stage;
     public static void home() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+    public static void logIn() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void chooseGenres() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chooseGenres.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }

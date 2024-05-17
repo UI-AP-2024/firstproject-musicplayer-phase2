0ace277ef2ac27566a05f7ccab64ab398da17b2c
Alireza <aliharadi10@gmail.com>
2024-05-17 13:13:47 +0330

    artistsList-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/ArtistsListController.java
@@ -0,0 +1,93 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class ArtistsListController {
+
+    @FXML
+    private Button artists_button;
+
+    @FXML
+    private Button audios_button;
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private GridPane gridPain;
+
+    @FXML
+    private Button home_button;
+
+    @FXML
+    private Button library_button;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button logout_button;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    private TextField search_Text;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
+    void artists_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void audios_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void library_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_Action(ActionEvent event) {
+
+    }
+
+}

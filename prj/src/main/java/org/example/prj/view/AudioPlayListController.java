c523df44084dc637389c6ad3d8a1d3b5863f2b44
Alireza <aliharadi10@gmail.com>
2024-05-17 13:05:31 +0330

    audioPlayList-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/AudioPlayListController.java
@@ -0,0 +1,37 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class AudioPlayListController {
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private GridPane gridPain;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    private TextField search_Text;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_Action(ActionEvent event) {
+
+    }
+
+}

fb09b31b52223fe5ad88f8ce5a84935f211d1116
Alireza <aliharadi10@gmail.com>
2024-05-17 12:34:38 +0330

    playMusic-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/PlayMusicController.java
@@ -0,0 +1,61 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+public class PlayMusicController {
+
+    @FXML
+    private Button addTo_button;
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private Text caption_text;
+
+    @FXML
+    private Circle image_circle;
+
+    @FXML
+    private ImageView likeRed_image;
+
+    @FXML
+    private ImageView likeWhite_image;
+
+    @FXML
+    private Text nameArtist_text;
+
+    @FXML
+    private Text name_text;
+
+    @FXML
+    private ScrollBar scroll;
+
+    @FXML
+    void addTo_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void likeRed_Action(MouseEvent event) {
+
+    }
+
+    @FXML
+    void likeWhite_Action(MouseEvent event) {
+
+    }
+
+}

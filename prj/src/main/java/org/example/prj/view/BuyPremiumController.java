58f5404b53be81a4020294e898a8a6676c51c9d5
Alireza <aliharadi10@gmail.com>
2024-05-17 12:07:21 +0330

    buyPremium-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/BuyPremiumController.java
@@ -0,0 +1,139 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class BuyPremiumController {
+
+    @FXML
+    private Button artists_button;
+
+    @FXML
+    private Button audios_button;
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private Button buy_button;
+
+    @FXML
+    private Text error_text;
+
+    @FXML
+    private Button home_button;
+
+    @FXML
+    private Button library_button;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button logout_button;
+
+    @FXML
+    private Text pkgOneName_text;
+
+    @FXML
+    private Text pkgThreeName_text;
+
+    @FXML
+    private Text pkgTwoName_text;
+
+    @FXML
+    private Text priceOne_text;
+
+    @FXML
+    private Text priceThree_text;
+
+    @FXML
+    private Text priceTwo_text;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private TextField search_Text;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
+    private Button selectOne_button;
+
+    @FXML
+    private Button selectThree_button;
+
+    @FXML
+    private Button selectTwo_button;
+
+    @FXML
+    void artists_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void audios_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buy_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void library_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectOne_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectThree_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectTwo_Action(ActionEvent event) {
+
+    }
+
+}

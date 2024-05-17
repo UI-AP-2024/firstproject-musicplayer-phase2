c7929b8a5483bc5f8c157d12031fd4da379b3c7c
Alireza <aliharadi10@gmail.com>
2024-05-17 12:14:37 +0330

    registerListener-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/RegisterLController.java
@@ -0,0 +1,116 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class RegisterLController {
+
+    @FXML
+    private Button artists_button;
+
+    @FXML
+    private Button audios_button;
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private TextField day_text;
+
+    @FXML
+    private TextField email_text;
+
+    @FXML
+    private Text error_text;
+
+    @FXML
+    private Button home_button;
+
+    @FXML
+    private Button library_button;
+
+    @FXML
+    private Button login_Button;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button logout_button;
+
+    @FXML
+    private TextField month_text;
+
+    @FXML
+    private TextField name_text;
+
+    @FXML
+    private TextField name_text1;
+
+    @FXML
+    private TextField number_text;
+
+    @FXML
+    private TextField password_text;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private TextField search_Text;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
+    private TextField year_text;
+
+    @FXML
+    void artists_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void audios_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void library_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_Action(ActionEvent event) {
+
+    }
+
+}

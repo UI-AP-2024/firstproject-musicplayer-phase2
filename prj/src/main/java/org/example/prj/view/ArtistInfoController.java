948ec1736591441ed16cb1156c5cf5e9dd1612eb
Alireza <aliharadi10@gmail.com>
2024-05-17 13:42:37 +0330

    artistInfo-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/ArtistInfoController.java
@@ -0,0 +1,136 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ArtistInfoController {
+
+    @FXML
+    private Text Email_text;
+
+    @FXML
+    private Button albums_button;
+
+    @FXML
+    private Button artists_button;
+
+    @FXML
+    private Button audios_button;
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private Text bio_text;
+
+    @FXML
+    private Text birth_text;
+
+    @FXML
+    private Text error_text;
+
+    @FXML
+    private Button follow_button;
+
+    @FXML
+    private Button followers_button;
+
+    @FXML
+    private Button home_button;
+
+    @FXML
+    private Button library_button;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button logout_button;
+
+    @FXML
+    private Text name_text;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private Button report_button;
+
+    @FXML
+    private TextField search_Text;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
+    private Text userName_text;
+
+    @FXML
+    void albums_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void artists_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void audios_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void follow_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void followers_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void library_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void report_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_Action(ActionEvent event) {
+
+    }
+
+}

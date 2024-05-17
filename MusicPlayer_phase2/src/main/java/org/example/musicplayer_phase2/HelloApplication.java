0fc23be0f27d31a99ac272153b9f3b27c9a404dc
fatemeh fahimpour <fatemefahimpoor1399@gmail.com>
2024-05-18 00:28:53 +0330

    test

--- MusicPlayer_phase2/src/main/java/org/example/musicplayer_phase2/HelloApplication.java
+++ MusicPlayer_phase2/src/main/java/org/example/musicplayer_phase2/HelloApplication.java
@@ -9,6 +9,7 @@ import org.example.musicplayer_phase2.controller.AboutLIstener.ListenerControlle
 import org.example.musicplayer_phase2.model.AboutHumans.*;
 import org.example.musicplayer_phase2.model.AboutMusic.Audio;
 import org.example.musicplayer_phase2.model.AboutMusic.Music;
+import org.example.musicplayer_phase2.model.Types.Free;
 import org.example.musicplayer_phase2.model.Types.Genre;
 import org.example.musicplayer_phase2.model.Types.Premium;
 
@@ -34,24 +35,14 @@ public class HelloApplication extends Application {
 
     public static final Admin admin = getAdmin("Fatemeh" , "fatemeh" , "1234!@#$" , "fatemefahimpoour1399@gmail.com" , "09131111111" , LocalDate.of(2005 , 12 ,12));
     public static void main(String[] args) throws Exception {
-        Audio audio1 = new Music("first" , "" , "" , Genre.POP , "https://ts1.tarafdari.com/contents/user141862/content-sound/richard_clayderman_-_mariage_d_amour.mp3" , "" , "");
-        Audio audio2 = new Music("abc" , "" , "" , Genre.POP , "https://dl.musicdel.ir/Music/1400/12/andy_williams_love_story.mp3" , "" , "");
-//        Audio audio3 = new Music("first" , "" , "" , Genre.HIPHOP , "" , "" , "");
-//        Audio audio4 = new Music("mmm" , "" , "" , Genre.COUNTRY, "" , "" , "");
-//        Audio audio5 = new Music("g" , "" , "" , Genre.POP , "" , "" , "");
-//        Audio audio6 = new Music("eeee" , "" , "" , Genre.ROCK , "" , "" , "");
-//        audio3.setLikesNum(100);
-//        audio5.setLikesNum(500);
+        Audio audio1 = new Music("1" , "" , "" , Genre.POP , "https://ts1.tarafdari.com/contents/user141862/content-sound/richard_clayderman_-_mariage_d_amour.mp3" , "" , "");
+        Audio audio2 = new Music("2" , "" , "" , Genre.POP , "https://dl.musicdel.ir/Music/1400/12/andy_williams_love_story.mp3" , "" , "");
+        Audio audio3 = new Music("3" , "" , "" , Genre.HIPHOP , "http://www.coca.ir/wp-content/uploads/2013/06/pirate-of-caribbean.mp3" , "" , "");
 
-        Artist artist1 = new Singer("fatemeh" , "first" , "" , " " , "" , LocalDate.now() , "");
-        Artist artist12 = new Podcaster("fatemeh" , "ftm" , "" , " " , "" , LocalDate.now() , "");
+        Artist artist1 = new Singer("fatemeh" , "fahim" , "1234!@#$" , "f@gmail.com" , "09131111111" , LocalDate.now() , "I don't have biography");
+        Artist artist12 = new Podcaster("maryam" , "mary" , "1234!@#$" , "m@gmail.com" , "09131111111" , LocalDate.now() , "");
 
-
-        Premium listener = new Premium("fatemeh" , "fahim" , "1234!@#$" , "" , "" , LocalDate.now());
-        ListenerController listenerController = new ListenerController();
-
-        listenerController.followingArtist(artist1 , listener);
-        listenerController.followingArtist(artist12 , listener);
+        Free listener = new Free("zahra" , "zahra" , "1234!@#$" , "" , "" , LocalDate.now());
 
         launch();
     }

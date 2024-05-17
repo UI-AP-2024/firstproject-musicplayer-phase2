e95b2e1fda0043ff6bfc0adcd5a604746733bdb5
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-05-18 03:18:49 +0330

    gridpane ke har khane , yek vbox darad va dar har vbox , cover audio va esme khode audio va esme artistash gharar darad

---
+++ spotifySecondFase/src/main/java/org/example/spotifysecondfase/view/AllAudios.java
@@ -0,0 +1,103 @@
+package org.example.spotifysecondfase.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import org.example.spotifysecondfase.model.Audio.Audio;
+import org.example.spotifysecondfase.model.Database;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AllAudios implements Initializable {
+    @FXML
+    private Button allArtists;
+    public Button getAllArtists() {return allArtists;}
+    public void setAllArtists(Button allArtists) {this.allArtists = allArtists;}
+    @FXML
+    private Button allAudios;
+    public Button getAllAudios() {return allAudios;}
+    public void setAllAudios(Button allAudios) {this.allAudios = allAudios;}
+    @FXML
+    private Label allAudiosLbl;
+    public Label getAllAudiosLbl() {return allAudiosLbl;}
+    public void setAllAudiosLbl(Label allAudiosLbl) {this.allAudiosLbl = allAudiosLbl;}
+    @FXML
+    private AnchorPane anchorPane;
+    public AnchorPane getAnchorPane() {return anchorPane;}
+    public void setAnchorPane(AnchorPane anchorPane) {this.anchorPane = anchorPane;}
+    @FXML
+    private GridPane gridPane;
+    public GridPane getGridPane() {return gridPane;}
+    public void setGridPane(GridPane gridPane) {this.gridPane = gridPane;}
+    @FXML
+    private ImageView homeImage;
+    public ImageView getHomeImage() {return homeImage;}
+    public void setHomeImage(ImageView homeImage) {this.homeImage = homeImage;}
+    @FXML
+    private ImageView homeLibrary;
+    public ImageView getHomeLibrary() {return homeLibrary;}
+    public void setHomeLibrary(ImageView homeLibrary) {this.homeLibrary = homeLibrary;}
+    @FXML
+    private VBox homeVbox;
+    public VBox getHomeVbox() {return homeVbox;}
+    public void setHomeVbox(VBox homeVbox) {this.homeVbox = homeVbox;}
+    @FXML
+    private ImageView searchImage;
+    public ImageView getSearchImage() {return searchImage;}
+    public void setSearchImage(ImageView searchImage) {this.searchImage = searchImage;}
+    @FXML
+    private ImageView spotify;
+    public ImageView getSpotify() {return spotify;}
+    public void setSpotify(ImageView spotify) {this.spotify = spotify;}
+    public VBox vBox(Audio audio)
+    {
+        String fontFamily = "Arial";
+        double fontSize = 12;
+        FontWeight fontWeight = FontWeight.SEMI_BOLD;
+        Font font=Font.font(fontFamily, fontWeight, fontSize);
+        Font font2 = Font.font(fontFamily,fontSize);
+        VBox vBox = new VBox();
+        Label labelVbox = new Label();
+        labelVbox.setFont(font);
+        Label labelVbox2 = new Label();
+        labelVbox2.setFont(font2);
+        ImageView imageView = audio.getCoverImageView();
+        imageView.setFitWidth(80);
+        imageView.setFitHeight(80);
+        labelVbox.setText(audio.getName());
+        labelVbox2.setText(audio.getArtistName());
+        vBox.getChildren().addAll(imageView,labelVbox,labelVbox2);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(10);
+        return vBox;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        int counter = 0;
+        for (int i=0; i<getGridPane().getRowCount(); i++)
+        {
+            for (int j=0; j<getGridPane().getColumnCount(); j++)
+            {
+                if (Database.getDatabase().getAudio().size() > counter)
+                {
+                    VBox vBox = vBox(Database.getDatabase().getAudio().get(counter));
+                    getGridPane().add(vBox,i,j);
+                    vBox.setOnMouseClicked(event -> {
+                        //ChangeScene.playMusic
+                    });
+                    counter++;
+                }
+            }
+        }
+    }
+}

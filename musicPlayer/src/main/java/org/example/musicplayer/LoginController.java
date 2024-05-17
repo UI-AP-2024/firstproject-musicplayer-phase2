55796ded83b98bf099986bd46cee9038f50814b8
mohsen <im.mohsen.369@gmail.com>
2024-05-17 23:54:08 +0430

    some edits on login page

--- musicPlayer/src/main/java/org/example/musicplayer/LoginController.java
+++ musicPlayer/src/main/java/org/example/musicplayer/LoginController.java
@@ -7,6 +7,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -14,29 +16,46 @@ import org.example.musicplayer.controller.SignInOutController;
 
 public class LoginController implements Initializable {
     @FXML
+    private TextField tf_username;
+
+    @FXML
+    private PasswordField pf_pass;
+
+    @FXML
+    private TextField tf_pass_visible;
+
+    @FXML
+    private CheckBox cb_showPass;
+
+    @FXML
+    private Button btn_login;
+
+    @FXML
+    private Button btn_signIn;
+    @FXML
     private AnchorPane anchor;
 
     @FXML
     private TextField tf_pass;
 
     @FXML
-    private TextField tf_username;
-    @FXML
-    private Button btn_login;
-    @FXML
-    private Button btn_signIn;
-
-    @FXML
-    void login(ActionEvent event) {
-        System.out.println(SignInOutController.getUserAccountController().login(tf_username.getText(), tf_pass.getText()));
-    }
-    @FXML
-    void signIn(ActionEvent event) {
-
-    }
-
-    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Bind the text of the PasswordField and the visible TextField
+        tf_pass_visible.textProperty().bindBidirectional(pf_pass.textProperty());
+    }
 
+    @FXML
+    private void togglePasswordVisibility() {
+        if (cb_showPass.isSelected()) {
+            tf_pass_visible.setVisible(true);
+            tf_pass_visible.setManaged(true);
+            pf_pass.setVisible(false);
+            pf_pass.setManaged(false);
+        } else {
+            tf_pass_visible.setVisible(false);
+            tf_pass_visible.setManaged(false);
+            pf_pass.setVisible(true);
+            pf_pass.setManaged(true);
+        }
     }
 }

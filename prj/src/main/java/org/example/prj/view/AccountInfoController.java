2e6ec191c0406a5a92f33997a0013c5974b29ab2
Alireza <aliharadi10@gmail.com>
2024-05-17 12:04:12 +0330

    accountInfo-view.fxml

---
+++ prj/src/main/java/org/example/prj/view/AccountInfoController.java
@@ -0,0 +1,145 @@
+package org.example.prj.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class AccountInfoController {
+
+    @FXML
+    private Button addPLayList_button;
+
+    @FXML
+    private Button artists_button;
+
+    @FXML
+    private Button audios_button;
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private Text birth_text;
+
+    @FXML
+    private Text credit_text;
+
+    @FXML
+    private Text daySub_text;
+
+    @FXML
+    private Text email_text;
+
+    @FXML
+    private Text error_text;
+
+    @FXML
+    private Button following_button;
+
+    @FXML
+    private Text genre_text;
+
+    @FXML
+    private Button home_button;
+
+    @FXML
+    private Button library_button;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button logout_button;
+
+    @FXML
+    private Text name_text;
+
+    @FXML
+    private Text phone_text;
+
+    @FXML
+    private Button playLists_button;
+
+    @FXML
+    private Button premium_button;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private TextField search_Text;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
+    private Text userName_text;
+
+    @FXML
+    void addPlayList_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void artists_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void audios_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void following_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void library_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playLIsts_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void premium_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_Action(ActionEvent event) {
+
+    }
+
+}

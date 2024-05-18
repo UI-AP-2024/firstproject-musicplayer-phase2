04997c4eb5558920f0d9c0e89441854e39a2a896
mohsen <im.mohsen.369@gmail.com>
2024-05-19 00:32:01 +0430

    base page edits...

--- musicPlayer/src/main/java/org/example/musicplayer/BasePageController.java
+++ musicPlayer/src/main/java/org/example/musicplayer/BasePageController.java
@@ -1,11 +1,79 @@
 package org.example.musicplayer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BasePageController implements Initializable {
+
+    @FXML
+    private Button btn_artists;
+
+    @FXML
+    private Button btn_audios;
+
+    @FXML
+    private Button btn_home;
+
+    @FXML
+    private Button btn_lib;
+
+    @FXML
+    private Button btn_login;
+
+    @FXML
+    private Button btn_logout;
+
+    @FXML
+    private Button btn_search;
+
+    @FXML
+    private Button btn_signIn;
+
+    @FXML
+    void btn_artists_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_audios_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_home_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_lib_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_login_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_logout_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_search_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_signIn_action(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 

037d548642a5f3eb1e5b653e913e12b9c32ca434
mohsen <im.mohsen.369@gmail.com>
2024-05-18 08:35:50 +0430

    main page fxml & controller

---
+++ musicPlayer/src/main/java/org/example/musicplayer/MainPageController.java
@@ -0,0 +1,14 @@
+package org.example.musicplayer;
+
+import javafx.fxml.Initializable;
+
+import java.lang.annotation.Inherited;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPageController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}

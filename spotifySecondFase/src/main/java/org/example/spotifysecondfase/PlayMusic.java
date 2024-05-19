3005546b8eee899d233f7978f17462c145d6c7f3
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-05-19 05:05:31 +0330

    safhe baraye pakhsh audio ke cover,name,artistName ra neshan midahad va mitan audio ra like kard ya audio ro add to playList kard

---
+++ spotifySecondFase/src/main/java/org/example/spotifysecondfase/PlayMusic.java
@@ -0,0 +1,76 @@
+package org.example.spotifysecondfase;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayMusic implements Initializable {
+    @FXML
+    private ImageView addToPlaylistImage;
+
+    @FXML
+    private Button allArtists;
+
+    @FXML
+    private Button allAudios;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Label artistName;
+
+    @FXML
+    private Label audioName;
+
+    @FXML
+    private ImageView coverOfAudio;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeImage;
+
+    @FXML
+    private ImageView homeLibrary;
+
+    @FXML
+    private VBox homeVbox;
+
+    @FXML
+    private ImageView likeImage;
+
+    @FXML
+    private ImageView lyrics;
+
+    @FXML
+    private VBox namesVbox;
+
+    @FXML
+    private ImageView nextIcon;
+
+    @FXML
+    private ImageView pauseIcon;
+
+    @FXML
+    private ImageView previousIcon;
+
+    @FXML
+    private ImageView searchImage;
+
+    @FXML
+    private ImageView spotify;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}

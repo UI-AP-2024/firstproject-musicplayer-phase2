5bec96cc06d8db589bcd46995ee5571b82db6838
Alireza <aliharadi10@gmail.com>
2024-05-21 08:53:26 +0330

    create a class for MediaPlayer

--- prj/src/main/java/module-info.java
+++ prj/src/main/java/module-info.java
@@ -1,16 +1,10 @@
 module org.example.prj {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.media;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
-    opens org.example.prj to javafx.fxml;
     exports org.example.prj;
+    exports org.example.prj.view to javafx.fxml;
+
+    opens org.example.prj.view to javafx.fxml;
 }
